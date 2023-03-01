6c8f6c6d6b36c9745230e1451a79cb10f88927b6
nguyenquocvu <nguyenquocvu696@gmail.com>
2023-03-01 21:59:27 +0700

    test setting repo, update template

---
+++ src/javaTester/Topic_01_System.java
@@ -0,0 +1,17 @@
+package javaTester;
+
+import org.apache.commons.exec.OS;
+
+public class Topic_01_System {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+		
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
+
+	}
+
+}
--- src/webdriver/Topic_02_WebDriver.java
+++
@@ -1,7 +0,0 @@
-package webdriver;
-
-public class Topic_02_WebDriver {
-	public static void main(String[] args) {
-		System.out.println("Update topic 2");
-	}
-}
